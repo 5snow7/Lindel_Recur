238f2ac0f281a1c9268e97318cba2f9faf92634a
mark <snowtowind@yahoo.com>
2018-11-10 16:36:50 -0800

    added link to TheList

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 let inplind,inplen,inpang;
-let par;
+let par,thelist;
 function start(){
 inplind=createInput("enter string for lind");
 inplind.class('inpbdd');inplind.position(950,60);
@@ -14,6 +14,9 @@ inpang.class('inpbdd');inpang.position(950,170);
 inpang.changed(chgang);	
 par=createP("enter string, len, angle and hit 'r'");
 par.class('inpbdd');par.position(950,-10);
+
+thelist=select('#tl');thelist.class('inpbdd');
+thelist.position(950,220);
 }
 
 function chgstr(){
